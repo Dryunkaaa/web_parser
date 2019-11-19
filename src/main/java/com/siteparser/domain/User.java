bdf3e39b6769181ea9f258116ba3ef5986ff83f4
Dryunkaaa <the_quemal@mail.ru>
2019-11-19 21:41:22 +0200

    Added date fields to user

--- src/main/java/com/siteparser/domain/User.java
+++ src/main/java/com/siteparser/domain/User.java
@@ -1,9 +1,11 @@
 package com.siteparser.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -22,6 +24,29 @@ public class User {
 
     @Column(name = "password")
     private String password;
+
+    @Column(name = "created_date")
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
+    @Temporal(TemporalType.DATE)
+    private Date createdDate;
+
+    @Column(name = "created_time")
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="HH:mm:ss")
+    @Temporal(TemporalType.TIME)
+    private Date createdTime;
+
+    @Column(name = "last_modified_by")
+    private String lastModifiedBy;
+
+    @Column(name = "last_modified_date")
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
+    @Temporal(TemporalType.DATE)
+    private Date lastModifiedDate;
+
+    @Column(name = "last_modified_time")
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="HH:mm:ss")
+    @Temporal(TemporalType.TIME)
+    private Date lastModifiedTime;
 
     @Column(name = "name")
     private String name;
